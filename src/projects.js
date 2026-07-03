d80d814b18b20fe4ccd5a39acf4b4da5d025929a
DyllanBaker246 <dyllanbakerpanda@gmail.com>
2026-07-02 21:44:40 -0700

    done for today

--- src/projects.js
+++ src/projects.js
@@ -18,16 +18,40 @@ class Project{
         `;
     }
 }
-
+const portfolioWebsite = new Project(
+    "Portfolio Website",
+    "Created a static website from scratch in order to show my portfolio. Also served as an oportunity to learn various scripting languages.",
+    ["HTML", "CSS", "JavaScript", "Web Dev"]);
 const certificateAuthority = new Project(
     "Certificate Authority",
+    "Created a Certificate Authority that issues certificates to Apache, Microsoft IIS, and Node.js web servers.",
+    ["PKI", "Digital Signatures", "X.509", "Certificate Chains", "Apache", "Node.js"]);
+const virtualPrivateNetwork = new Project(
+    "Virtual Private Network",
+    "Secured a connection between two devices using an encrypted tunnel.", 
+    ["WireGuard", "Linux"]);
+const passwordlessSSH = new Project(
+    "Passwordless SSH",
+    "Set up a passwordless SSH into a Linux system using public key cryptography.", 
+    ["SSH", "Public Key Cryptography", "Linux"]);
+const IPSecChannel = new Project(
+    "IPSec Channel",
     "lorem emsum",
     ["meow", "meow"]);
-const testProj2 = new Project(
-    "title2",
+const prettyGoodPrivacy = new Project(
+    "Pretty Good Privacy",
+    "lorem emsumjsdhfjksd jlksdjf lksdjflksdjf sbfdkhsd kjhdfskd jrhfjkdsfhkdgfjfhkjdf,fj", 
+    ["bark", "bark"]);
+const DNSSECBasedDNSServer = new Project(
+    "DNSSEC-Based DNS Server",
     "lorem emsumjsdhfjksd jlksdjf lksdjflksdjf sbfdkhsd kjhdfskd jrhfjkdsfhkdgfjfhkjdf,fj", 
     ["bark", "bark"]);
 
 
+document.getElementById("Portfolio Website").innerHTML = portfolioWebsite.displayCard();
 document.getElementById("Certificate Authority").innerHTML = certificateAuthority.displayCard();
-document.getElementById("project 2").innerHTML = testProj2.displayCard();
+document.getElementById("Virtual Private Network").innerHTML = virtualPrivateNetwork.displayCard();
+document.getElementById("Passwordless SSH").innerHTML = passwordlessSSH.displayCard();
+document.getElementById("IPSec Channel").innerHTML = IPSecChannel.displayCard();
+document.getElementById("Pretty Good Privacy").innerHTML = prettyGoodPrivacy.displayCard();
+document.getElementById("DNSSEC-Based DNS Server").innerHTML = DNSSECBasedDNSServer.displayCard();
